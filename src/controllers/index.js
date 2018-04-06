dad0ff2161b95d478673d21fc4228e048dbfd080
Oberzan <oberzan.simon@gmail.com>
2018-04-06 15:06:16 +0200

    Moved googleCOlors to the controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,28 @@ const jwt = require('jsonwebtoken');
 
 const ballotService = require('../services/ballot');
 const voteService = require('../services/vote');
+const googleColors = [
+  '#3366CC',
+  '#DC3912',
+  '#FF9900',
+  '#109618',
+  '#990099',
+  '#3B3EAC',
+  '#0099C6',
+  '#DD4477',
+  '#66AA00',
+  '#B82E2E',
+  '#316395',
+  '#994499',
+  '#22AA99',
+  '#AAAA11',
+  '#6633CC',
+  '#E67300',
+  '#8B0707',
+  '#329262',
+  '#5574A6',
+  '#3B3EAC'
+];
 
 homepage = (req, res, next) => {
   ballotService.getBallots()
@@ -23,6 +45,7 @@ homepage = (req, res, next) => {
         title: "HyperVote",
         ballots: ballots,
         ballotsOptions: ballotsOptions,
+        googleColors,
         i18n: res
       });
     }); 
