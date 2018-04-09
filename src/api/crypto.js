88656ad6704a429c027e3fcf6c0ab9002620c348
Oberzan <oberzan.simon@gmail.com>
2018-04-09 09:12:49 +0200

    Refactor crypto.js

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -1,19 +1,14 @@
 const crypto = require('crypto');
 
-var returnJsonResponse = (response, status, content) => {
-    response.status(status);
-    response.json(content);
-};
-
-module.exports.hashToken = function(request, response) {
-    console.log(request.body)
-    if(request.body.token) {
-        var obj = request.body.token;
-        var hash = crypto.createHash('sha256').update(obj, 'utf8').digest().toString('hex');
-        console.log(hash);
-        returnJsonResponse(response, 200, hash);
-    } else {
-        returnJsonResponse(response, 400, "Nothing to hash");
-    }
+module.exports.hashToken = function(req, res) {
+  console.log(req.body)
+  if(req.body.token) {
+    var obj = req.body.token;
+    var hash = crypto.createHash('sha256').update(obj, 'utf8').digest().toString('hex');
+    console.log(hash);
+    res.status(200).json(hash);
+  } else {
+    res.status(400).json("Nothing to hash");
+  }
 }
     
