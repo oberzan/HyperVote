aa8669e22f20d598c7a2ef888188381bacc145c5
Oberzan <oberzan.simon@gmail.com>
2018-04-10 10:25:41 +0200

    Set permissions for API calls

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let guard = require('express-jwt-permissions')()
 
-var crypto = require('../api/crypto');
-var ballot = require('../api/ballot')
+let crypto = require('../api/crypto');
+let ballot = require('../api/ballot')
 
 
 router.post('/hash/token', crypto.hashToken);
 
-router.post('/ballot', ballot.createBallot)
-router.delete('/ballot/:id', ballot.delete);
+router.post('/ballot', guard.check('ADMIN'), ballot.createBallot)
+router.delete('/ballot/:id', guard.check('ADMIN'), ballot.delete);
 
-router.post('/ballot/:id/tokens', ballot.createTokens);
+router.post('/ballot/:id/tokens', guard.check('ADMIN'), ballot.createTokens);
 
 router.get('/ballot/:id/results', ballot.getResults);
 
