74c70b7cee6fd7ab0c5814b2ad05394a129e5ceb
Oberzan <oberzan.simon@gmail.com>
2018-04-11 14:59:03 +0200

    Make logger format same for all transports

--- log.js
+++ log.js
@@ -2,37 +2,38 @@ const { createLogger, format, transports } = require('winston');
 const util = require('util');
 
 const getLogger = (callingModule) => {
+  let fileArr = callingModule.filename.split('/');
+  let file = callingModule.parent.parent !== null ? fileArr[fileArr.length -2] + '/' : '';
+  file = file + fileArr.pop();
+
   let logger = createLogger({
     level: 'info',
-    format: format.simple(),
+    format: format.combine(
+      format.colorize(),
+      format.timestamp(),
+      format.label({label: file}),
+      //format.align(),
+      format.printf((info) => {
+        const {
+          timestamp, level, message, label, ...args
+        } = info;
+        const ts = timestamp.slice(0, 19).replace('T', ' ');
+        let logInfo = `${ts} [${level}]: [${label}]`;
+        let nSpaces = logInfo.length > 64 ? 0 : 64 - logInfo.length;
+        logInfo += Array(nSpaces+1).join(' ');
+        
+        return `${logInfo} ${util.format(message)} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+      })
+    ),
     transports: [
-      // - Write to all logs with level `info` and below to `combined.log`
-      // - Write all logs error (and below) to `error.log`.
       new transports.File({ filename: 'logs/error.log', level: 'error' }),
       new transports.File({ filename: 'logs/combined.log' })
     ]
   });
 
   if (process.env.NODE_ENV !== 'production') {    
-    let fileArr = callingModule.filename.split('/');
-    let file = callingModule.parent.parent !== null ? fileArr[fileArr.length -2] + '/' : '';
-    file = file + fileArr.pop();
     logger.add(new transports.Console({
-      level: 'debug',
-      format: format.combine(
-        format.colorize(),
-        format.timestamp(),
-        format.label({label: file}),
-        //format.align(),
-        format.printf((info) => {
-          const {
-            timestamp, level, message, label, ...args
-          } = info;
-          const ts = timestamp.slice(0, 19).replace('T', ' ');
-          return `${ts} [${level}]: [${label}] ${util.format(message)} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
-        })
-      )
-        
+      level: 'debug'
     }));
   }
   return logger;
